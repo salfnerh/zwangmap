3615ca04f4a3a7b61683ed0ff1ceda7cc0c3697b
Henry <zwangmagazin@gmail.com>
2021-04-12 23:01:30 +0200

    neue ordnerstruktur, bisschen aufgeräumt. hoffe klappt noch alles lol

--- Tileset_32x32_bruno.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Tileset_32x32_bruno" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="Tileset_32x32_bruno.png" width="320" height="320"/>
-</tileset>
