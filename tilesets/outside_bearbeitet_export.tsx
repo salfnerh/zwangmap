a1f8d60d66fbcca9bab87d4890aae30aa2cef764
Betaisnot <65882022+Betaisnot@users.noreply.github.com>
2021-04-20 03:53:33 +0000

    Deploying to gh-pages from master @ 9e441a3414dc69865c942a3735239ebfa4279ad7 🚀

---
+++ tilesets/outside_bearbeitet_export.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="527-5276245_map-tile-v3-rpg-tileset_bearbeitet" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="667" columns="23">
+ <image source="527-5276245_map-tile-v3-rpg-tileset_bearbeitet.png" width="760" height="958"/>
+</tileset>
